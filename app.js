aeeb2e0cc3d3c6886fdc6c66fae627e944fb17b0
Alex Gilbert <alex@punkave.com>
2012-05-18 17:42:21 -0400

    Added environmental variable for port.

--- app.js
+++ app.js
@@ -66,5 +66,5 @@ app.get('/images', function(req, res) {
   loadImages(req, res);
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
