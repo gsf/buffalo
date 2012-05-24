860de6c00d9e203037d8cef35f9ec9f42b1c2c5e
Alex Gilbert <alex@punkave.com>
2012-05-23 20:43:36 -0400

    Trying various oauth stuff to work with the Dropbox API.

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var express = require('express')
   , fs      = require('fs')
   , im      = require('imagemagick')
   , redis   = require('redis-url').connect(process.env.REDISTOGO_URL)
-  , dropbox = require('dropbox').DropboxClient;
+  , dropbox = require('dropbox').DropboxClient
+  , oauth   = require('oauth-client');
 
 var app = module.exports = express.createServer();
 
@@ -88,6 +89,40 @@ function sendImages(req, res, imageMeta) {
 
 app.get('/images', function(req, res) {
   loadParams(req, res);
+});
+
+app.get('/authorize', function(req, res) {
+  var key = '0as9n56d4x5str5';
+  var secret = 'j4b4h7274obs32r';
+  var consumer = oauth.createConsumer(key, secret);
+  var signer = oauth.createHmac(consumer);
+
+  // get a Request Token from dropbox
+  var request = {
+      host: 'api.dropbox.com',
+      path: '/1/1/oauth/request_token',
+      oauth_consumer_key: key,
+      oauth_signature_method: key,
+      oauth_signature: signer,
+      oauth_timestamp: key,
+      oauth_nonce: key,
+      method: 'POST',
+      https: true,
+      port: 443,
+  }
+
+  console.log(request);
+  request = oauth.request(request, function(response) {
+    if (err) throw err;
+
+    console.log(response);
+  });
+
+  // redirect user to dropbox authorization with the Request Token
+
+  // accept user back from their authorization on dropbox
+  // request an Access Token from dropbox
+  // store the Access Token in Redis
 });
 
 app.listen(process.env.PORT || 3000);
